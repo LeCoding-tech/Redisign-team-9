70c870d661f4384a2f1f84587ff28dd5abfd93c8
Wilson Peres <r2h@adminisatorsmbp.home>
2020-06-22 10:51:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
-import React from 'react';
-import './App.css';
-import HomePage from './Components/HomePage/homePage'
 
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
-}
+import React, { Component } from 'react';
+import productData from  './Components/ProductsBody/ProductsData.json';
+import Products from './Components/ProductsBody/ProductsBody';
+class App extends Component {
+    render() {
+      return (
+        <Products/>
+      );
+    }
+  }
 
+
+
+
+
+
+//   constructor(){
+//     super();
+
+//     this.state={
+//       search:null
+//     };
+//   }
+
+//   searchSpace=(event)=>{
+//     let keyword = event.target.value;
+//     this.setState({search:keyword})
+//   }
+
+//   render(){
+//     const styleInfo = {
+//       paddingRight:'10px',
+//     }
+
+//     const elementStyle ={
+//       border:'solid',
+//       borderRadius:'10px',
+//       position:'relative',
+//       left:'10vh',
+//       height:'3vh',
+//       width:'20vh',
+//       marginTop:'5vh',
+//       marginBottom:'10vh'
+//     }
+
+//     const items = productData.filter((productData)=>{
+//       if(this.state.search == null)
+//           return productData
+//       else if(productData.productname.toLowerCase().includes(this.state.search.toLowerCase()) || productData.description.toLowerCase().includes(this.state.search.toLowerCase())){
+//           return productData
+//       }
+//     })
+//     .map(productData=>{
+//       return(
+//       <div>
+//         <ul>
+//           <li style={{position:'relative',left:'10vh'}}>
+//             <span style={styleInfo}>{productData.productname}</span>
+//           </li>
+//         </ul>
+//       </div>
+//       )
+//     })
+
+//     return (
+//       <div>
+//       <input 
+//       type="text" 
+//       placeholder="Enter item to be searched"
+//       style={elementStyle} 
+//       onChange={(e)=>this.searchSpace(e)} />
+
+//       {items}
+//       </div>
+//     )
+// }
+// }
 export default App;
--- src/Components/ProductsBody/ProductsBody.js
+++ src/Components/ProductsBody/ProductsBody.js
@@ -1,10 +1,138 @@
 import React from 'react';
 import './productsBody.css';
+import Product from './ProductCards';
+import productData from './ProductsData.json';
+import '../ProductsBody/productsBody.css';
 
-const ProductsBody = () => {
-    <>
 
-    </>
-}
+const initialState = {
+    products: productData
+  }
+  // this.state is our brain 
+  class Products extends React.Component {
 
-export default ProductsBody;
+    constructor() {
+      super()
+      this.state = initialState
+    }
+
+    
+  
+  filterProducts =(evt) => {
+    let value = evt.target.value
+  
+    // makes a new array for all products
+    const allProducts =  [...initialState.products] 
+      if (value === "All"){
+        this.setState({products: allProducts})
+        return 
+      }
+      // makes a new array for product availability 
+      //filters by category
+    const filterProducts = allProducts.filter(product => {
+      return product.category === value
+    })
+    // only products that we filter
+    this.setState({products: filterProducts})
+  };
+  
+  
+  filterPrice =(evt) => {
+    let value = evt.target.value
+  
+    // makes a new array for product availability 
+    const allProducts =  [...initialState.products] 
+      if (value === "All"){
+        this.setState({products: allProducts})
+        return 
+      }
+      //filters by price
+    const filterPrice = allProducts.filter(product => {
+      return product.price === value
+    })
+    // only products that we filter
+    this.setState({products: filterPrice})
+  };
+
+
+  searchSpace=(evt)=>{
+    let keyword = evt.target.value;
+    this.setState({search:keyword})
+  }
+  
+ 
+  
+  render(){
+    const styleInfo = {
+      paddingRight:'10px',
+    }
+
+    const elementStyle ={
+      border:'solid',
+      borderRadius:'10px',
+      position:'relative',
+      left:'10vh',
+      height:'3vh',
+      width:'20vh',
+      marginTop:'5vh',
+      marginBottom:'10vh'
+    }
+
+    const items = productData.filter((productData)=>{
+      if(this.state.search == null)
+          return productData
+      else if(productData.productname.toLowerCase().includes(this.state.search.toLowerCase()) || productData.description.toLowerCase().includes(this.state.search.toLowerCase())){
+          return productData
+      }
+    })
+
+  const mappedProducts = this.state.products.map(product => {
+     
+      return <Product name={product.productname} price={product.price} description={product.description} image={product.image} />
+  })
+  
+
+
+  
+      return (
+
+        
+  
+    <div className="ProductsContainer">
+      <h1 >Mobile Products</h1>
+      <div>
+     <input 
+      type="text" 
+      placeholder="Enter item to be searched"
+      style={elementStyle} 
+      onChange={(e)=>this.searchSpace(e)} />
+
+      </div>
+
+        <select onChange={evt => this.filterProducts(evt)} >
+                <option value="All">All Types</option>
+                <option value="PersonalAudio">Headphones</option>
+                 <option value="MobilePhones">Smart Phones</option>
+                 <option value="Tablets">Tablets</option>
+                 <option value="Watches">Watches</option>
+                 <option value="Laptops">Laptops</option>
+        </select> 
+  
+        <select onChange={evt => this.filterPrice(evt)} >
+                <option value="All">All Prices</option>
+                <option value="5.00">$5.00</option>
+                <option value="7.00">$7.00</option>
+                <option value="10.00">$10.00</option>
+                <option value="12.00">$12.00</option>
+        </select> 
+  
+     <div className= "container">
+     {mappedProducts}
+    </div>   
+    </div>
+  );
+  }
+  }
+
+  
+  export default Products;
