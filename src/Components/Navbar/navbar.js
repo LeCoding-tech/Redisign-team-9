16b963d0fc39d0b294b70f69aab61f0b1eb20345
Naven Kennedy <r2h@AdminisatorsMBP.attlocal.net>
2020-06-22 11:33:50 -0400

    navbarandfooterchanged

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
 import './navbar.css'
 
 const Navbar = () => {
-    <>
-
-    </>
+    return (
+        <div>
+           
+                <nav id="navbar">
+                <img className = "droid_nav" alt="android" src = "img/Droid.png"/>
+                    <ul> 
+                    
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Features</li>
+                        <li>Pricing</li>
+                    </ul>
+                    </nav>
+            </div>
+           
+       
+    )
 }
 
 export default Navbar;
