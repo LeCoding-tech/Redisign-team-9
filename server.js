6f4fbf67eb88601a250f95e4a0ba4bd60086e817
LeCoding-tech <59509041+LeCoding-tech@users.noreply.github.com>
2020-06-29 13:57:03 -0400

    deleting extra files

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-var router = require('./src/Components/Routes/apiProducts')
+var router = require('./Routes/apiProducts')
 
 app.use(express.json());
 app.use(cors())
 
 app.use("/api")
 
-const port = process.env.SERVER || 3007;
+const port = process.env.SERVER || 3006;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
