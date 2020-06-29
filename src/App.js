cc1dbd393ce11fdf1d3bc6565ff061ab4cdb4ce7
LeCoding-tech <59509041+LeCoding-tech@users.noreply.github.com>
2020-06-29 15:45:29 -0400

    updating database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import Routes from './Components/Routes/Router'
+import Routes from './Components/Router/Router'
 class App extends Component {
     render() {
       return (
