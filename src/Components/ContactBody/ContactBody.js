7b5eb9cb02ece0330927ac36d8d098ecc571fbae
FosterCareAwareness <51935764+FosterCareAwareness@users.noreply.github.com>
2020-06-30 11:25:10 -0400

    Contact Changes

--- src/Components/ContactBody/ContactBody.js
+++ src/Components/ContactBody/ContactBody.js
@@ -27,20 +27,24 @@ class ContactBody extends React.Component {
         const { userId, title, body } = this.state
         return (
             <>
+            <div className="contactMaincontainer">
                 <h1 className="heading">Contact Us if You Have Any Questions</h1>
-                <form classsName="myForm" onSubmit={this.submitHandler} >
-                    <label class="label--center">Name</label>
-                    <input type="text" placeholder="Enter Name Here..." name="name" id="name--position" value="name" />
-                    <label class="label--center">Email</label>
-                    <input type="text" placeholder="Enter Email Here..." name="email" id="email--position" value="email" />
-                    <label class="label--center">Comments</label>
-                    <textarea rows="4" cols="50" placeholder="Enter comments Here..." id="comments--position"></textarea>
-                    <div class="container">
-                        <button class="button--item" type="submit" >Submit</button>
-                        <input type="reset" class="button--item" value="comment" />
-                    </div>
-                </form>
-                <Maps />
+        <form className="myForm">
+          <label className="label--center">Name:</label>
+          <input type="text" placeholder="Enter Name Here..." name="name" className="name--position" />
+          <label className="label--center">Email:</label>
+          <input type="text" placeholder="Enter Email Here..." name="email" className="email--position" />
+          <label className="label--center">Comments:</label>
+          <textarea rows="4" cols="50" placeholder="Enter comments Here..." className="comments--position"></textarea>
+          <div className="button--div">
+            <button className="button--item">Submit</button>
+            <input type="reset" className="button--item" />
+            </div>
+        </form>
+        
+                <h1>Find our locations</h1>
+        <Maps />
+                </div>
                 
             </>
         )
