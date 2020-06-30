ef9f05fd3f467c1afed3ac0a74da0a587b9d77d4
Naven Kennedy <r2h@AdminisatorsMBP.attlocal.net>
2020-06-30 10:38:29 -0400

    changes added

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -6,38 +6,39 @@ const Footer = () => {
         <div className="box bg-dark">
             {/* <div className ="img_box">
             </div> */}
-            <img className="footer_samsung" alt="android" src="img/Samsung2.jpg" />
+            <Link to="/" className="footer_box1">
+                <img className="footer_samsung" alt="android" src="img/Samsung2.jpg" />
+            </Link>
             <div className="footer_box1">
 
-            <Link to="/" className="link">
 
-                <h3>Home</h3>
-            </Link>
-            <Link to="/Products" className="link">
-                <h3>Products</h3>
-            </Link>
-            <Link to="/Contact" className="link">
-                <h3>Contact Us</h3>
-            </Link>
+                <Link to="/">
+                    <h3 className="link">Home</h3>
+                </Link>
+                <Link to="/Products" >
+                    <h3 className="link">Products</h3>
+                </Link>
+                <Link to="/Contact">
+                    <h3 className="link">Contact Us</h3>
+                </Link>
 
-           </div>
+            </div>
 
 
 
 
             <div className="footer_box2">
-            <Link to="/" className="link">
-            <h3>Sign Up</h3> 
-            </Link>
-                
-                <h3>Personas</h3>
+                <Link to="/" >
+                    <h3 className="link">Sign Up</h3>
+                </Link>
+
+                <h3 className="link">Personas</h3>
 
             </div>
             <div className="social_media">
-                <h3>Feel Free To Check Out Our Social Media Sites!!!</h3>
-                <a href="https://www.instagram.com/samsungmobile/?hl=en" rel="noopener"> <img className="instagram_footer" alt="IG" src="img/instagram.png" /> </a>
-                <a href="https://twitter.com/samsung?lang=en" rel="noopener"><img className="twitter_footer" src="img/twitter.png" /></a>
-                <a href="https://www.facebook.com/SamsungUS/" rel="noopener"> <img className="facebook_footer" alt="Facebook" src="img/facebook.jpg" /></a>
+                <a href="https://www.instagram.com/samsungmobile/?hl=en" rel="noopener" target="_blank"> <img className="instagram_footer" alt="IG" src="img/instagram.png" /> </a>
+                <a href="https://twitter.com/samsung?lang=en" rel="noopener" target="_blank"><img className="twitter_footer" src="img/twitter.png" /></a>
+                <a href="https://www.facebook.com/SamsungUS/" rel="noopener" target="_blank"> <img className="facebook_footer" alt="Facebook" src="img/facebook.jpg" /></a>
                 <h3>Samsung &copy; 2020, All Rights Reserved </h3>
             </div>
         </div>
