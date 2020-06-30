509dabe29c55617b831bd517ca7167899bd8c83b
LeCoding-tech <59509041+LeCoding-tech@users.noreply.github.com>
2020-06-30 16:06:45 -0400

    removing file

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-var router = require('./Routes/apiProducts')
+let router = require('./Routes/apiProducts')
 
 app.use(express.json());
 app.use(cors())
 
-app.use("/api")
 
 const port = process.env.SERVER || 3006;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+
+app.use('/api', router)
