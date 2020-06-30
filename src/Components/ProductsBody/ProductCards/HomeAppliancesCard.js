70edfb7e741a7bf2b720a5da9b9b8b173ea20f5e
Wilson Peres <r2h@adminisatorsmbp.home>
2020-06-30 11:29:54 -0400

    css names

--- src/Components/ProductsBody/ProductCards/HomeAppliancesCard.js
+++ src/Components/ProductsBody/ProductCards/HomeAppliancesCard.js
@@ -45,19 +45,19 @@ class HomeApplianceCard extends React.Component {
     }
   }
 
-//  HomeApplianceCard = ({ name, description, price, image }) => {
+//  const HomeApplianceCard = ({ name, description, price, image }) => {
 //     return (
-        // <div className="card">
-        //     <img src={image} alt="Image of product" className="MenuImg"/>
-        //     <div className="container2">
-        //         <h3 className="ProductName">{name}</h3>
-        //         <div className="descriptionBox">
-        //         <p className="Description">{description}</p>
-        //         </div>
-        //         <p className="price">${price}</p>
-        //         <button className="ProductButton " type="button">Buy Now</button>
-        //     </div>
-        // </div>
+//         <div className="card">
+//             <img src={image} alt="Image of product" className="MenuImg"/>
+//             <div className="container2">
+//                 <h3 className="ProductName">{name}</h3>
+//                 <div className="descriptionBox">
+//                 <p className="Description">{description}</p>
+//                 </div>
+//                 <p className="price">${price}</p>
+//                 <button className="ProductButton " type="button">Buy Now</button>
+//             </div>
+//         </div>
 //     )
 // }
 export default HomeApplianceCard;
