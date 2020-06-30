fd30a0ce12657444fa5571ec5796210ddf0fe295
Nicolas Conaway <n.conaway00@gmail.com>
2020-06-30 16:20:37 -0400

    persona adjusted

--- src/Components/Persona/persona.js
+++ src/Components/Persona/persona.js
@@ -7,28 +7,28 @@ const Persona = () => {
     return (
         <>
             <div className="PersonaBox">
-            <div className="Personacontainer">
-                <img className="imgPersona" src={David} alt="Picture of Customer" />
-                <p><span>David Garcia</span> Road 2 Hire Student</p>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star"></span>
-                <p>Story: David enjoys technology and working with new appliances. David enjoys our website redesign and how easy it was to use it. David was able to find the products he needed and was able to contact our customer support if there were any issues he encountered. David remains a loyal customer until this day.</p>
+                <div className="Personacontainer">
+                    <img className="imgPersona" src={David} alt="Picture of Customer" />
+                    <p><span>David Garcia</span> Road 2 Hire Student</p>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <p>Story: David enjoys technology and working with new appliances. David enjoys our website redesign and how easy it was to use it. David was able to find the products he needed and was able to contact our customer support if there were any issues he encountered. David remains a loyal customer until this day.</p>
+                </div>
+                <div className="Personacontainer">
+                    <img className="imgPersona" src={Alan} alt="Picture of Customer" />
+                    <p><span>Alan Vang</span>Front end engineer</p>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <p>Story: Alan was one of our customers looking for new mobile products to use and found the redesign of our site very user friendly. “The site was very easy to use. Not complicated and I was able to have an easy process buying the applications I needed.” ~ Alan</p>
+                </div>
             </div>
-            <div className="Personacontainer">
-                <img className="imgPersona" src={Alan} alt="Picture of Customer" />
-                <p><span>Alan Vang</span>Front end engineer</p>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <p>Story: Alan was one of our customers looking for new mobile products to use and found the redesign of our site very user friendly. “The site was very easy to use. Not complicated and I was able to have an easy process buying the applications I needed.” ~ Alan</p>
-            </div>
-            </div>
-           
+
         </>
     )
 }
